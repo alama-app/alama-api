7e77aaff6292007df8dc994c80fc228afae58989
samwel141 <samwelwilson141@gmail.com>
2024-08-25 10:07:59 +0300

    fix swagger9

--- index.js
+++ index.js
@@ -1,25 +1,92 @@
+// const express = require('express');
+// const connectDB = require('./config/db');
+// const dotenv = require('dotenv');
+// const cors = require('cors');
+// const morgan = require('morgan');
+// const bodyParser = require('body-parser');
+
+// import swaggerJSDoc from 'swagger-jsdoc';
+
+// const CSS_URL =
+//   "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css";
+
+// const businessOwnerRoutes = require('./routes/businessOwnerRoutes');
+// const businessRoutes = require('./routes/businessRoutes');
+// const staffRoutes = require('./routes/staffRoutes');
+// const foodRoutes = require('./routes/foodRoutes'); 
+// const fruitRoutes = require('./routes/fruitRoutes');
+// const addonRoutes = require('./routes/addonRoutes');
+// const hotDrinkRoutes = require('./routes/hotDrinkRoutes'); 
+// const softDrinkRoutes = require('./routes/softDrinkRoutes');
+// const orderRoutes = require('./routes/orderRoutes');
+// const setupSwagger = require('./swagger').default;
+
+// dotenv.config();
+// const app = express();
+// app.use(bodyParser.json());
+// app.use(cors());
+// app.use(morgan("dev"));
+
+// connectDB();
+
+// app.use(express.json());
+
+// setupSwagger(app);
+
+
+// app.get('/', (req, res) => {
+//     res.send('Welcome to Alama App API!');
+// });
+
+// const specs = swaggerJSDoc(options);
+
+// app.use(
+//     "/api-docs",
+//     swaggerUI.serve,
+//     swaggerUI.setup(specs, { customCssUrl: CSS_URL })
+//   );
+
+// app.get('/swagger.json', (req, res) => {
+//     res.setHeader('Content-Type', 'application/json');
+//     res.send(swaggerSpec);
+// });
+
+
+// // Routes
+// app.use('/api/business_owners', businessOwnerRoutes);
+// app.use('/api/businesses', businessRoutes);
+// app.use('/api/staffs', staffRoutes);
+// app.use('/api/foods', foodRoutes);
+// app.use('/api/fruits', fruitRoutes);
+// app.use('/api/addons', addonRoutes); 
+// app.use('/api/hotDrinks', hotDrinkRoutes);
+// app.use('/api/softDrinks', softDrinkRoutes); 
+// app.use('/api/orders', orderRoutes); 
+
+// const PORT = process.env.PORT || 4000;
+
+// app.listen(PORT, () => {
+//     console.log(`Server is running on port ${PORT}`);
+// });
+
+
+
+
+
+
+
+
+
+
+
 const express = require('express');
 const connectDB = require('./config/db');
 const dotenv = require('dotenv');
 const cors = require('cors');
-const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const morgan = require('morgan');
 
-import swaggerJSDoc from 'swagger-jsdoc';
-
-const CSS_URL =
-  "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css";
-
-const businessOwnerRoutes = require('./routes/businessOwnerRoutes');
-const businessRoutes = require('./routes/businessRoutes');
-const staffRoutes = require('./routes/staffRoutes');
-const foodRoutes = require('./routes/foodRoutes'); 
-const fruitRoutes = require('./routes/fruitRoutes');
-const addonRoutes = require('./routes/addonRoutes');
-const hotDrinkRoutes = require('./routes/hotDrinkRoutes'); 
-const softDrinkRoutes = require('./routes/softDrinkRoutes');
-const orderRoutes = require('./routes/orderRoutes');
-const setupSwagger = require('./swagger').default;
+const setupSwagger = require('./swagger');
 
 dotenv.config();
 const app = express();
@@ -33,24 +100,9 @@ app.use(express.json());
 
 setupSwagger(app);
 
-
 app.get('/', (req, res) => {
     res.send('Welcome to Alama App API!');
 });
-
-const specs = swaggerJSDoc(options);
-
-app.use(
-    "/api-docs",
-    swaggerUI.serve,
-    swaggerUI.setup(specs, { customCssUrl: CSS_URL })
-  );
-
-app.get('/swagger.json', (req, res) => {
-    res.setHeader('Content-Type', 'application/json');
-    res.send(swaggerSpec);
-});
-
 
 // Routes
 app.use('/api/business_owners', businessOwnerRoutes);
--- swagger.js
+++ swagger.js
@@ -1,7 +1,44 @@
-// const swaggerJSDoc = require('swagger-jsdoc');
-// const swaggerUi = require('swagger-ui-express');
+// // const swaggerJSDoc = require('swagger-jsdoc');
+// // const swaggerUi = require('swagger-ui-express');
 
-// // Basic Meta Information about our API
+// // // Basic Meta Information about our API
+// // const options = {
+// //     definition: {
+// //         openapi: '3.0.0',
+// //         info: {
+// //             title: 'Alama App API',
+// //             version: '1.0.0',
+// //             description: 'API documentation for the Alama App API',
+// //         },
+// //         servers: [
+// //             {
+// //                 // url: 'http://localhost:4000/',
+// //                 url: 'https://alama-api.vercel.app/',
+// //             },
+// //         ],
+// //     },
+// //     apis: ['./routes/*.js', './models/*.js'], 
+// // };
+
+// // const swaggerSpec = swaggerJSDoc(options);
+
+// // const setupSwagger = (app) => {
+// //     app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+// // };
+
+// // module.exports = setupSwagger;
+
+
+
+
+
+
+// import swaggerJSDoc from 'swagger-jsdoc';
+// import swaggerUi from 'swagger-ui-express';
+// import swaggerUiDist from 'swagger-ui-dist';
+// import path from 'path';
+
+
 // const options = {
 //     definition: {
 //         openapi: '3.0.0',
@@ -12,72 +49,73 @@
 //         },
 //         servers: [
 //             {
-//                 // url: 'http://localhost:4000/',
 //                 url: 'https://alama-api.vercel.app/',
+//                 description: "My API Documentation",
 //             },
 //         ],
 //     },
-//     apis: ['./routes/*.js', './models/*.js'], 
+//     // apis: ['./routes/*.js', './models/*.js'], 
+//     apis: ["src/**/*.js"],
 // };
 
 // const swaggerSpec = swaggerJSDoc(options);
 
+// // const setupSwagger = (app) => {
+// //     app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec, {
+// //         swaggerOptions: {
+// //             url: '/swagger.json' 
+// //         },
+// //         customCssUrl: `${swaggerUiDist.getAbsoluteFSPath()}/swagger-ui.css`,
+// //         customJsUrl: `${swaggerUiDist.getAbsoluteFSPath()}/swagger-ui-bundle.js`,
+// //     }));
+// // };
+
 // const setupSwagger = (app) => {
-//     app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+//     const specs = swaggerJSDoc(options);
+//     app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(specs, { customCssUrl: CSS_URL }));
+
+//     app.get('/swagger.json', (req, res) => {
+//         res.setHeader('Content-Type', 'application/json');
+//         res.send(specs);
+//     });
 // };
 
-// module.exports = setupSwagger;
+// export default setupSwagger;
 
 
 
 
 
 
-import swaggerJSDoc from 'swagger-jsdoc';
-import swaggerUi from 'swagger-ui-express';
-import swaggerUiDist from 'swagger-ui-dist';
-import path from 'path';
 
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUI = require('swagger-ui-express'); // Import swagger-ui-express
 
 const options = {
     definition: {
-        openapi: '3.0.0',
+        openapi: "3.0.0",
         info: {
-            title: 'Alama App API',
-            version: '1.0.0',
-            description: 'API documentation for the Alama App API',
+            title: "API Documentation",
+            version: "1.0.0",
+            description: "API Documentation for Alama App",
         },
         servers: [
             {
-                url: 'https://alama-api.vercel.app/',
-                description: "My API Documentation",
+                url: "http://localhost:4000",
             },
         ],
     },
-    // apis: ['./routes/*.js', './models/*.js'], 
-    apis: ["src/**/*.js"],
+    apis: ["./routes/*.js"], // Path to the API docs
 };
 
-const swaggerSpec = swaggerJSDoc(options);
+const swaggerSpec = swaggerJsDoc(options);
 
-// const setupSwagger = (app) => {
-//     app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec, {
-//         swaggerOptions: {
-//             url: '/swagger.json' 
-//         },
-//         customCssUrl: `${swaggerUiDist.getAbsoluteFSPath()}/swagger-ui.css`,
-//         customJsUrl: `${swaggerUiDist.getAbsoluteFSPath()}/swagger-ui-bundle.js`,
-//     }));
-// };
-
-const setupSwagger = (app) => {
-    const specs = swaggerJSDoc(options);
-    app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(specs, { customCssUrl: CSS_URL }));
-
+function setupSwagger(app) {
+    app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerSpec));
     app.get('/swagger.json', (req, res) => {
         res.setHeader('Content-Type', 'application/json');
-        res.send(specs);
+        res.send(swaggerSpec);
     });
-};
+}
 
-export default setupSwagger;
+module.exports = setupSwagger;
